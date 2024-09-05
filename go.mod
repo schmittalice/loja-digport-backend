c715b639c18629da880824b19a78dda656e29ef3
alice <alice.schmittmachado@gmail.com>
2024-09-05 15:45:01 -0300

    cors added

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 )
--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"github.com/schmittalice/loja-digport-backend/controller"
 )
 
@@ -11,11 +12,20 @@ func HandleRequests() {
 
 	route := mux.NewRouter()
 
-	route.HandleFunc("/produtos", controller.BuscaProdutosHandler).Methods("GET")
+	route.HandleFunc("/produto", controller.BuscaProdutosHandler).Methods("GET")
 	route.HandleFunc("/produto", controller.BuscaProdutoPorNomeHandler).Methods("GET")
 	route.HandleFunc("/produto", controller.CriaProdutoHandler).Methods("POST")
 	route.HandleFunc("/produto/{id}", controller.RemoveProdutoHandler).Methods("DELETE")
 	//route.HandleFunc("/produto", controllers.AtualizaProdutoHandler).methods("PUT")
 
-	http.ListenAndServe(":8080", route)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "PUT"},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(route)
+
+	http.ListenAndServe(":8080", handler)
 }
