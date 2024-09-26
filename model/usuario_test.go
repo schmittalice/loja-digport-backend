c5ed6874288fb617c68aef812ec2b750e2229f25
alice <alice.schmittmachado@gmail.com>
2024-09-25 21:09:03 -0300

    corrige usuario _test

--- model/usuario_test.go
+++ model/usuario_test.go
@@ -6,28 +6,51 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidausuario(t *testing.T) {
-	t.Run("Deve retonar nulo quando usuario não tiver nome", func(t *testing.T) {
-		//arrange
-		usuario := Usuario{Nome: "", Email: "email", Senha: ""}
-		expectedErrorMessage := "nome do usuário não pode ser vazio"
+func TestValidaUsuario(t *testing.T) {
+	t.Run("Deve retornar nulo quando o cadastro do usuário estiver completo", func(t *testing.T) {
+		// arange
+		usuario := Usuario{Nome: "fulano", Email: "ashd", Senha: "senha"}
 
-		//act
+		// act
 		err := ValidaUsuario(usuario)
 
-		//assert
-		assert.NoError(t, err, expectedErrorMessage)
+		// assert
+		assert.NoError(t, err)
 	})
 
-	t.Run("Deve retonar nulo quando usuario não tiver nome", func(t *testing.T) {
-		//arrange
-		usuario := Usuario{Nome: "", Email: "email", Senha: ""}
-		expectedErrorMessage := "nome do usuário não pode ser vazio"
+	t.Run("Deve retornar erro quando usuário não tiver senha", func(t *testing.T) {
+		// arange
+		usuario := Usuario{Nome: "fulano", Email: "ashd", Senha: ""}
+		expectedErrorMessage := "Senha do usuário deve ser preenchida"
 
-		//act
+		// act
 		err := ValidaUsuario(usuario)
 
-		//assert
-		assert.NoError(t, err, expectedErrorMessage)
+		// assert
+		assert.EqualError(t, err, expectedErrorMessage)
+	})
+
+	t.Run("Deve retornar erro quando usuário não tiver nome", func(t *testing.T) {
+		// arange
+		usuario := Usuario{Nome: "", Email: "ashd", Senha: "sdasd"}
+		expectedErrorMessage := "Nome do usuário deve ser preenchida"
+
+		// act
+		err := ValidaUsuario(usuario)
+
+		// assert
+		assert.EqualError(t, err, expectedErrorMessage)
+	})
+
+	t.Run("Deve retornar erro quando usuário não tiver email", func(t *testing.T) {
+		// arange
+		usuario := Usuario{Nome: "fulano", Email: "", Senha: "sdasd"}
+		expectedErrorMessage := "E-mail do usuário deve ser preenchida"
+
+		// act
+		err := ValidaUsuario(usuario)
+
+		// assert
+		assert.EqualError(t, err, expectedErrorMessage)
 	})
 }
